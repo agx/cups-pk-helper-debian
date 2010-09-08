370c2e42c527bdc58cd6e5529a556aeec75dc132
Vincent Untz <vuntz@novell.com>
2010-09-08 19:30:35 +0200

    Add HACKING, update README
    Also update auto autogen.sh to be closer to autogen.sh from my GNOME
    modules.

--- autogen.sh
+++ autogen.sh
@@ -4,18 +4,19 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="CUPS PolicyKit helper"
+PKG_NAME="cups-pk-helper"
+REQUIRED_AUTOMAKE_VERSION=1.9
+REQUIRED_M4MACROS=
 
 (test -f $srcdir/configure.ac \
-  && test -f $srcdir/src/cups-pk-helper-mechanism.c) || {
+  && test -f $srcdir/$PKG_NAME.doap) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level package directory"
+    echo " top-level $PKG_NAME directory"
     exit 1
 }
 
-
 which gnome-autogen.sh || {
-    echo "You need to install gnome-common from a package or from GNOME git."
+    echo "You need to install gnome-common."
     exit 1
 }
 
